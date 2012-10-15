e0f79a833e84b65fdd9e69bf22f43ba68a799afe
John Kelly <jkelly0@me.com>
2012-10-14 22:58:02 -0700

    Show trial countdown on manage page

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,4 +1,12 @@
 class Subscriber < ActiveRecord::Base
   has_many :users, dependent: :destroy
   has_one  :subscription, dependent: :destroy
+
+  def trial?
+    ((Time.now - created_at.to_time) < 1.month.to_i) && subscription.try(:plan_id) == 1
+  end
+
+  def days_left_in_trial
+    ((1.month.to_i - (Time.now - created_at.to_time)) / 1.day.to_i).round
+  end
 end
--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -5,4 +5,27 @@ describe Subscriber do
     it { should have_many(:users).dependent(:destroy) }
     it { should have_one(:subscription).dependent(:destroy) }
   end
+
+  describe "trial?" do
+    let!(:subscription) { create(:basic_subscription) }
+
+    it "returns true if plan is 1 and subscriber is less than a month old" do
+      subscription.subscriber.stub(:created_at).and_return(1.day.ago.to_time)
+      subscription.subscriber.trial?.should be_true
+    end
+
+    it "returns fase if subscriber is more than a month old" do
+      subscription.subscriber.stub(:created_at).and_return(30.days.ago.to_time)
+      subscription.subscriber.trial?.should be_false
+    end
+  end
+
+  describe "days_left_in_trial" do
+    let!(:subscription) { create(:basic_subscription) }
+
+    it "returns the number of days before the trial expires" do
+      subscription.subscriber.stub(:created_at).and_return(1.day.ago.to_time)
+      subscription.subscriber.days_left_in_trial.should == 29
+    end
+  end
 end
