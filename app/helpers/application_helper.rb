6d298a31d35a06bdea39a51c82256a706e2a1a0f
John Kelly <jkelly0@me.com>
2012-10-15 19:21:19 -0700

    Say 30 day trial for signing up for plan 1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,8 @@ module ApplicationHelper
       end
     elsif current_user
       link = link_to "Add Billing Info to Subscribe", new_customer_path, class: "btn btn-success", data: { "no-turbolink" => true }
+    elsif plan_id == 1
+      link = link_to "Start Free 30 Day Trial", new_user_registration_path, class: "btn btn-success"
     end
     link
   end
